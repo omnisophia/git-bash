7a8ef46149da2515fb0953656018970da3e88380
life.is.lavish@gmail.com <life.is.lavish@gmail.com>
2024-05-07 02:15:11 +0000

    Added bash commentary for github credit.

--- git.sh
+++ git.sh
@@ -3,6 +3,9 @@ read -p "Commit description: " desc
 git add . && \
 git add -u && \
 git commit -m "$desc" && \
-git push origin "$branch" && \
-echo "Git Update is complete. Please continue to push using 'git push' once you are ready."
+git push origin "$branch" || echo "Git Update is complete. Please continue to push using 'git push' once you are ready."
 
+##################################################################################################
+##				Brought to you buy Omnisophia, Inc.				##
+##			       https://github.com/omnisophia/git-bash				##
+##################################################################################################
